e778f12ed32ef7f8170c91ce3d34d84c44e5f410
carner <1643120032@qq.com>
2013-08-12 17:28:17 +0800

    页面显示效果。

---
+++ style.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	
+</head>
+<body>
+	<!电影演员表效果!>
+	<div style="position:absolute;width:240px;height:200px;visibility:show; z-index:10;left:310px;top:30px;background-color:lightyellow; filter:alpha(opacity=85);"> 
+		<table border=1 background="bg_100.jpg"> 
+			<tr> 
+				<td width=240> 
+					<marquee style="height:200px;text-align:center;left:0px;top:1px;color:red; margin-left:1" scrollamount=1 behavior=loop direction=up> 
+						网站建设单位：国瑞公司<br><br> 
+						网页制作：张 庆<br><br> 
+						时 间:二○○○年五月二十二日 
+					</marquee> 
+				</td> 
+			</tr> 
+		</table> 
+	</div>
+</body>
+</html>
